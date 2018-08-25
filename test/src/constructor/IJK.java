fc1b1837db0fcdcadbae5fe77304a37c39af0f91
madh <mad@gmail.com>
2018-08-25 16:28:05 +0530

    first commit of IJ and  xyz

---
+++ test/src/constructor/IJK.java
@@ -0,0 +1,23 @@
+package constructor;
+
+public class IJK {
+
+	int i,j,k;
+	IJK(int x)
+	{
+		i=x;
+		j=x*x;
+		k=i+j;
+	}
+	IJK()
+	{
+		System.out.println(i+j+k);
+	}
+	public static void main(String args[])
+	{
+		IJK m=new IJK(6);
+		System.out.println(m.i);
+		System.out.println(m.j);
+		System.out.println(m.k);
+	}
+}
---
+++ test/src/constructor/XYZ.java
@@ -0,0 +1,16 @@
+package constructor;
+
+public class XYZ {
+
+	int i,j,k;
+	public static void main(String args[])
+	{
+		XYZ x1=new XYZ();
+		x1.i=30;
+		x1.j=40;
+		x1.k=60;
+		System.out.println(x1.i);
+		System.out.println(x1.j);
+		System.out.println(x1.k);
+	}
+}
