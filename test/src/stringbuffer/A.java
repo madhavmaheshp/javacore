16508af099abd55331cd269fd4f50fcc4d55bf1f
madh <mad@gmail.com>
2018-08-27 13:00:11 +0530

    String buffer

---
+++ test/src/stringbuffer/A.java
@@ -0,0 +1,13 @@
+package stringbuffer;
+
+public class A {
+
+	public static void main(String[] args) {
+		String s1="xy";
+		StringBuffer sb=new StringBuffer("abc");
+		System.out.println(s1);
+		System.out.println(6);
+		System.out.println(s1+6+8+9);
+	}
+
+}
