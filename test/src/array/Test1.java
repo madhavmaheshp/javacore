2b57d57246deda78b533f2d642b006f6099a0020
madh <mad@gmail.com>
2018-08-27 21:00:12 +0530

    Last commit

---
+++ test/src/array/Test1.java
@@ -0,0 +1,12 @@
+package array;
+
+public class Test1 {
+
+	public static void main(String args[])
+	{
+		String s[]= {"abc"};
+		A.main(s);
+		System.out.println(">>>>>>><<<<<<<");
+		A.main(null);
+	}
+}
